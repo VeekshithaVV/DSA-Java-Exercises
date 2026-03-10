557df1bed9ce95e8013b950750985e77af3b7ff6
Veekshitha V V <veekshithavv1408@gmail.com>
2026-03-10 23:15:52 +0530

    First & Last Pos Element in Sorted Arary

--- B_Week2/A_First_Last_Pos.java
+++ B_Week2/A_First_Last_Pos.java
@@ -7,5 +7,6 @@ import java.util.*;
 public class A_First_Last_Pos {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Do the things now,After is a Regret!!!");
     }
 }
