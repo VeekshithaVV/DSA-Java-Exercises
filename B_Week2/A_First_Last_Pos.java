2d5fb26d7c9c797cbdba5bbd00ae8d2395a640b7
Veekshitha V V <veekshithavv1408@gmail.com>
2026-03-10 23:14:08 +0530

    First & Last Pos Element in Sorted Arary

---
+++ B_Week2/A_First_Last_Pos.java
@@ -0,0 +1,11 @@
+//Find First and Last Position of Element in Sorted Array
+
+package B_Week2;
+
+import java.util.*;
+
+public class A_First_Last_Pos {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+    }
+}
